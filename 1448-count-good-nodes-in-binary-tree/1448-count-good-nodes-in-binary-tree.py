e286c8810dd216f63539f38a32a595fbb7b91230
Harsh Santwani <96873014+HydrallHarsh@users.noreply.github.com>
2026-04-22 22:51:03 +0530

    Time: 172 ms (5.27%), Space: 31.8 MB (71.66%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -8,27 +8,17 @@ class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         count = 0
         currpath = []
-        maps = {}
-        gmax = 0
         def dfs(root,path):
             nonlocal count
             if not root:
-                # if currpath:
-                #     currpath.pop()
                 return
-            # if gmax =
             currpath.append(root.val)
-            # maps[tuple(currpath)] = max(currpath)
-            # print(root.val, currpath, maps)
             if max(currpath) <= root.val:
-                # print("i am in")
                 count +=1
-            # if root.val > max()
             dfs(root.left,currpath)
             dfs(root.right,currpath)
             if currpath:
                 currpath.pop()
-            # return count
             
         dfs(root,currpath)
         
